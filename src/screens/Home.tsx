48d152e46f6f2dc371c975a2fe67cc36b4a79216
Gabriel Biankati <gabriel.2023014161@alunocesul.com>
2024-08-23 15:38:21 -0300

    add input and button

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,35 @@
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, TextInput, TouchableOpacity, TouchableOpacityBase, View } from "react-native"
 
 export function Home() {
+
+  function handleProductAdd() {
+    console.log('Teste')
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerText}>Lista de Compras </Text>
       </View>
-        <View>
 
+        <View style={styles.form}>
+          <TextInput 
+            style={styles.input}
+            placeholder="Adicione um novo produto"
+            placeholderTextColor='#808080'
+            keyboardType="default"
+          />
+
+          <TouchableOpacity
+          style={styles.button}
+          onPress={handleProductAdd}
+          >
+            <Text style={styles.textButton}>
+              +
+            </Text>
+          </TouchableOpacity>
         </View>
+
     </View>
   )
 }
@@ -28,5 +49,38 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#F2F2F2',
     margin: 'auto'
+  },
+  form: {
+    flexDirection: 'row',
+    width: '100%',
+    marginTop: -30
+  },
+  input: {
+    flex: 1,
+    backgroundColor: '#F2F2F2',
+    borderRadius: 6,
+    padding: 16,
+    alignItems: 'center',
+    fontSize: 16,
+    marginRight: 5,
+    height: 54,
+    marginLeft: 24,
+    borderColor: '#808080',
+    borderStyle: 'solid',
+    borderWidth: 0.5
+  },
+  button: {
+    borderRadius: 6,
+    backgroundColor: '#31C667',
+    width: 52,
+    height: 52,
+    borderColor: '#808080',
+    marginRight: 24,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textButton: {
+    color: '#fff',
+    fontSize: 24
   }
 })
